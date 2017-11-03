e4b55d060b7bf0105ebd7e2a18e6e2e9e19a8de8
Dominik Strohmeier <onkeltom@users.noreply.github.com>
2017-11-03 15:05:41 +0100

    Add files via upload

--- static/js/experiment.js
+++ static/js/experiment.js
@@ -5,24 +5,32 @@ var launch_funcs = {
     setTimeout(function() {
         $('#l1').addClass('active');
         $('#w1').css({'opacity': '1'});
+		$('#t1').css({'opacity': '1'});
+		$('#s1').css({'opacity': '1'});
         setTimeout(function() {
-            $('#s1').css({'opacity': '1'});
+            $('#search').css({'opacity': '1'})
             setTimeout(function() {
-                $('#t1').css({'opacity': '1'});
-                setTimeout(function() {
-                  $('#active-game').addClass('end');
-                  setTimeout(function() {
-                    $('#end-trial').click();
-                    $('#active-game').removeClass('end');
-                  }, 500);
-                }, 2000);
-            }, jsPsych.currentTrial().data.delay * (2/3));
-        }, jsPsych.currentTrial().data.delay * (1/3));
-    }, 200);
+                $('#tops').css({'opacity': '1'})
+	            setTimeout(function() {
+	                $('#po').css({'opacity': '1'})
+	                setTimeout(function() {
+	                  $('#active-game').addClass('end');
+	                  setTimeout(function() {
+	                    $('#end-trial').click();
+	                    $('#active-game').removeClass('end');
+	                  }, 500);
+	                }, 2000);
+				}, jsPsych.currentTrial().data.delay[3]);
+			}, 	jsPsych.currentTrial().data.delay[2]);
+       }, jsPsych.currentTrial().data.delay[1]);
+    }, jsPsych.currentTrial().data.delay[0]);
   },
   '2': () => {
     setTimeout(function() {
         $('#l1').addClass('active');
+        $('#w1').css({'opacity': '1'});
+		$('#t1').css({'opacity': '1'});
+		$('#s1').css({'opacity': '1'});
         setTimeout(function() {
             $('#w1').css({'opacity': '1'});
             $('#s1').css({'opacity': '1'});
@@ -34,7 +42,7 @@ var launch_funcs = {
                 $('#active-game').removeClass('end');
               }, 500);
             }, 2000);
-        }, jsPsych.currentTrial().data.delay);
+        }, jsPsych.currentTrial().data.delay[2]);
     }, 200);
   }
 }
